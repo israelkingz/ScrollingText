8557ccd6f15fad5591a2ddcc134e4fd191094c05
IsraelkingzDEV <IsraelkingzDEV>
2018-10-04 15:58:09 +0100

    Initial commit

---
+++ app/src/main/java/com/israelkingz/work/MainActivity.java
@@ -0,0 +1,22 @@
+package com.israelkingz.work;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * This app displays a scrollable TextView (a magazine article).
+ * All changes were made to the layout. No code changes.
+ * The code below is part of the Empty Activity template.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
